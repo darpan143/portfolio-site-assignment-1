92079b3235c1a3d79002e469216c8f9e401c4080
Darpan Nayyar <darpan.learning@gmail.com>
2024-06-09 21:43:05 -0400

    Header design change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.jpg';
 
 const Header = () => {
     return (
         <header className="App-header">
+            <img src={logo} alt="Logo" className="logo" />
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/professional-summary">Professional Summary</Link></li>
                     <li><Link to="/skills">Skills</Link></li>
                     <li><Link to="/education">Education</Link></li>
                     <li><Link to="/work-history">Work History</Link></li>
